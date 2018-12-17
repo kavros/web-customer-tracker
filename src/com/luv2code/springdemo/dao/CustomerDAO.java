a997f89f874c0fc58f23522e9003fcdb0a196474
kavros <akavroulakis@gmail.com>
2018-12-17 22:14:51 +0200

    add code for update, delete and search

--- src/com/luv2code/springdemo/dao/CustomerDAO.java
+++ src/com/luv2code/springdemo/dao/CustomerDAO.java
@@ -7,6 +7,14 @@ import com.luv2code.springdemo.entity.Customer;
 public interface CustomerDAO {
 	
 	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+
+	public List<Customer> searchCustomers(String theSearchName);
 	
 	
 }
--- src/com/luv2code/springdemo/service/CustomerService.java
+++ src/com/luv2code/springdemo/service/CustomerService.java
@@ -6,5 +6,13 @@ import com.luv2code.springdemo.entity.Customer;
 
 public interface CustomerService {
 	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+
+	public List<Customer> searchCustomers(String theSearchName);
 	
 }
